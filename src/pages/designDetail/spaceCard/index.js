dda936cd021d03ebad6a0ebf3061c73f6e73f786
Zhaochen <zhaochen01@juran.com.cn>
2018-12-25 10:15:52 +0800

    fix bug

--- src/pages/designDetail/spaceCard/index.js
+++ src/pages/designDetail/spaceCard/index.js
@@ -1,7 +1,19 @@
 import Taro, { Component } from "@tarojs/taro";
 import { View, Text, ScrollView, Image, Icon } from "@tarojs/components";
+import {fontColorShallow, fontWeightBold} from  '../../../constants';
+import Accordion from "../accordion";
+
 import "./style.scss";
 
+const filterByTypes = (renderImgs) => {
+    if(!renderImgs) return {panos:[],imgs:[]}
+    return renderImgs.reduce(({panos, imgs},curr)=>{
+          const {renderType} = curr;
+          if(renderType === 'image') return {panos, imgs:[...imgs,curr]};
+          else if (renderType === 'panorama') return {imgs, panos:[...panos, curr]}
+          return {panos, imgs};
+        },{panos:[],imgs:[]})
+  };
 class Card extends Component {
   static defaultProps = {
     space: {}
@@ -11,8 +23,20 @@ class Card extends Component {
     this.state = {};
   }
   render(){
-    const {space} = this.props;
-    return <View className='space-card'>{space}</View>
+    const {space:{description,renderImgs,roomTypeCode}} = this.props;
+    const {imgs: imags =[], panos: panoramas =[]} = filterByTypes(renderImgs)
+    console.log(imags, panoramas);
+    return <View className='space-card'>
+    <Accordion
+      title='空间描述'
+      renderHead={<View style={{fontWeight:fontWeightBold,textAlign:'left',color: fontColorShallow, height:'42rpx', lineHeight:'42rpx',fontSize:'30rpx'}}>{roomTypeCode}</View>}
+      disabled={!description}
+      buttonStyle={{lineHeight:'42rpx'}}
+    >
+        <View className='accordion-body'>{description}</View>
+      </Accordion>
+      
+    </View>
   }
 }
 export default Card;
